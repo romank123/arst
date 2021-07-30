0a613002de8121b9fb9ba3a1b47ef004983159e7
Roman <roman_kobeev@mail.ru>
2021-07-30 18:52:09 +0300

    all

---
+++ src/js/libs/zakazat.js
@@ -0,0 +1,121 @@
+
+//date picker
+const MONTH_NAMES = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+function app() {
+    return {
+        showDatepicker: false,
+        datepickerValue: '',
+
+        month: '',
+        year: '',
+        no_of_days: [],
+        blankdays: [],
+        days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+
+        initDate() {
+            let today = new Date();
+            this.month = today.getMonth();
+            this.year = today.getFullYear();
+            this.datepickerValue = new Date(this.year, this.month, today.getDate()).toDateString();
+        },
+
+        isToday(date) {
+            const today = new Date();
+            const d = new Date(this.year, this.month, date);
+
+            return today.toDateString() === d.toDateString() ? true : false;
+        },
+
+        getDateValue(date) {
+            let selectedDate = new Date(this.year, this.month, date);
+            this.datepickerValue = selectedDate.toDateString();
+
+            this.$refs.date.value = selectedDate.getFullYear() +"-"+ ('0'+ selectedDate.getMonth()).slice(-2) +"-"+ ('0' + selectedDate.getDate()).slice(-2);
+
+            console.log(this.$refs.date.value);
+
+            this.showDatepicker = false;
+        },
+
+        getNoOfDays() {
+            let daysInMonth = new Date(this.year, this.month + 1, 0).getDate();
+
+            // find where to start calendar day of week
+            let dayOfWeek = new Date(this.year, this.month).getDay();
+            let blankdaysArray = [];
+            for ( var i=1; i <= dayOfWeek; i++) {
+                blankdaysArray.push(i);
+            }
+
+            let daysArray = [];
+            for ( var i=1; i <= daysInMonth; i++) {
+                daysArray.push(i);
+            }
+
+            this.blankdays = blankdaysArray;
+            this.no_of_days = daysArray;
+        }
+    }
+};
+
+function range() {
+    return {
+        minprice: 3,
+        maxprice: 5,
+        min: 3,
+        max: 8,
+        minthumb: 3,
+        maxthumb: 8,
+
+        mintrigger() {
+            this.minprice = 3;
+            this.minthumb = 0;
+        },
+
+        maxtrigger() {
+            this.maxprice = Math.max(this.maxprice, this.minprice);
+            this.maxthumb = 100 - (((this.maxprice - this.min) / (this.max - this.min)) * 100);
+        },
+    }
+};
+
+//counter
+function decrement(e) {
+    const btn = e.target.parentNode.parentElement.querySelector(
+        'button[data-action="decrement"]'
+    );
+    const target = btn.nextElementSibling;
+    let value = Number(target.value);
+    value--;
+    target.value = value;
+}
+
+function increment(e) {
+    const btn = e.target.parentNode.parentElement.querySelector(
+        'button[data-action="decrement"]'
+    );
+    const target = btn.nextElementSibling;
+    let value = Number(target.value);
+    value++;
+    target.value = value;
+}
+
+const decrementButtons = document.querySelectorAll(
+    `button[data-action="decrement"]`
+);
+
+const incrementButtons = document.querySelectorAll(
+    `button[data-action="increment"]`
+);
+
+decrementButtons.forEach(btn => {
+    btn.addEventListener("click", decrement);
+});
+
+incrementButtons.forEach(btn => {
+    btn.addEventListener("click", increment);
+});
+
+
